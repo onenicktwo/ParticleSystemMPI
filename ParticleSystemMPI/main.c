002ba004c242fbc1c4eb8a0d01714f983b5faba3
onenicktwo <nmorrow619@gmail.com>
2024-12-03 23:37:46 -0600

    THIS WORKS, noticeable improvement on processors and doesn't crash

--- ParticleSystemMPI/main.c
+++ ParticleSystemMPI/main.c
@@ -1,63 +1,151 @@
-#include <GL/glut.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
-#include <Windows.h>
-#include "particle.h"
-#include "renderer.h"
-#include "globals.h"
-#include "mpi_handler.h"
+#include <GL/glut.h>
+#include <time.h>
+#include <math.h>
 
-int running = 1;
+#define NUM_PARTICLES 500000
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define FPS 60
 
-void cleanup() {
-    running = 0;
+typedef struct {
+    float x, y;
+    float vx, vy;
+} Particle;
+
+Particle* particles = NULL;
+Particle* local_particles = NULL;
+int num_particles_per_proc;
+int rank, size;
+double lastTime;
+int frameCount = 0;
+double fps = 0.0;
+
+void initParticles() {
+    if (rank == 0) {
+        particles = (Particle*)malloc(NUM_PARTICLES * sizeof(Particle));
+        if (particles == NULL) {
+            fprintf(stderr, "Failed to allocate memory for particles\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        for (int i = 0; i < NUM_PARTICLES; i++) {
+            particles[i].x = rand() % WINDOW_WIDTH;
+            particles[i].y = rand() % WINDOW_HEIGHT;
+            particles[i].vx = ((float)rand() / RAND_MAX - 0.5f) * 2.0f;
+            particles[i].vy = ((float)rand() / RAND_MAX - 0.5f) * 2.0f;
+        }
+    }
+}
+
+void updateParticles() {
+    for (int i = 0; i < num_particles_per_proc; i++) {
+        local_particles[i].x += local_particles[i].vx;
+        local_particles[i].y += local_particles[i].vy;
+
+        if (local_particles[i].x < 0 || local_particles[i].x > WINDOW_WIDTH) local_particles[i].vx *= -1;
+        if (local_particles[i].y < 0 || local_particles[i].y > WINDOW_HEIGHT) local_particles[i].vy *= -1;
+    }
+
+    // Gather all particles to the root process
+    MPI_Gather(local_particles, num_particles_per_proc * sizeof(Particle), MPI_BYTE,
+        particles, num_particles_per_proc * sizeof(Particle), MPI_BYTE,
+        0, MPI_COMM_WORLD);
+}
+
+// Display function for OpenGL
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_POINTS);
+    for (int i = 0; i < NUM_PARTICLES; i++) {
+        glVertex2f(particles[i].x / WINDOW_WIDTH * 2 - 1, particles[i].y / WINDOW_HEIGHT * 2 - 1);
+    }
+    glEnd();
+
+    frameCount++;
+    double currentTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
+    if (currentTime - lastTime > 1.0) {
+        fps = frameCount / (currentTime - lastTime);
+        frameCount = 0;
+        lastTime = currentTime;
+    }
+
+    char fpsString[30];
+    sprintf_s(fpsString, sizeof(fpsString), "FPS: %.2f", fps);
+
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glBegin(GL_QUADS);
+    glVertex2f(-0.98f, 0.85f);
+    glVertex2f(-0.98f, 0.95f);
+    glVertex2f(-0.7f, 0.95f);
+    glVertex2f(-0.7f, 0.85f);
+    glEnd();
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glRasterPos2f(-0.95f, 0.9f); 
+    for (char* c = fpsString; *c != '\0'; c++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
+    }
+
+    glutSwapBuffers();
+}
+
+// Timer function to control the update rate
+void timer(int value) {
+    updateParticles();
+    if (rank == 0) {
+        glutPostRedisplay();
+    }
+    glutTimerFunc(1000 / FPS, timer, 0);
 }
 
 int main(int argc, char** argv) {
-    // Initialize MPI
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Initialize MPI handler
-    initMPIHandler();
-
-    // Initialize particles (all processes)
-    initParticles();
+    num_particles_per_proc = (NUM_PARTICLES + size - 1) / size;
+    local_particles = (Particle*)malloc(num_particles_per_proc * sizeof(Particle));
+    if (local_particles == NULL) {
+        fprintf(stderr, "Failed to allocate memory for local_particles\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
 
     if (rank == 0) {
-        // Initialize GLUT
+        initParticles();
+    }
+
+    MPI_Scatter(particles, num_particles_per_proc * sizeof(Particle), MPI_BYTE,
+        local_particles, num_particles_per_proc * sizeof(Particle), MPI_BYTE,
+        0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
         glutInit(&argc, argv);
-        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-        glutInitWindowSize(window_width, window_height);
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+        glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         glutCreateWindow("Particle System");
 
-        // Set up display functions
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glPointSize(2.0f);
+
         glutDisplayFunc(display);
-        glutReshapeFunc(reshape);
-        glutKeyboardFunc(keyboard);
-        glutMouseFunc(mouse);
-        glutMotionFunc(mouseMotion);
-        glutTimerFunc(0, timer, 0);
+        glutTimerFunc(1000 / FPS, timer, 0);
+        lastTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
 
-        // Set clear color to black
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-        // Set up cleanup function
-        atexit(cleanup);
-
-        // Start main loop (only for rank 0)
         glutMainLoop();
     }
     else {
-        // Other processes enter a computation loop
-        while (running) {
+        while (1) {
             updateParticles();
-            Sleep(16); // Sleep for approximately 16ms (60 FPS)
         }
     }
 
-    // Finalize MPI
-    MPI_Type_free(&MPI_PARTICLE);
+    free(local_particles);
+    if (rank == 0) {
+        free(particles);
+    }
+
     MPI_Finalize();
     return 0;
 }
